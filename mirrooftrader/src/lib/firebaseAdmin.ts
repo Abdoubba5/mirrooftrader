df4ebec5bc3d0ebe38bdffddb52731f6020898d1
Abdoubba5 <158740943+Abdoubba5@users.noreply.github.com>
2026-01-19 16:43:23 +0100

    Update firebaseAdmin.ts

--- mirrooftrader/src/lib/firebaseAdmin.ts
+++ mirrooftrader/src/lib/firebaseAdmin.ts
@@ -1,19 +1,17 @@
-import { cert, getApps, initializeApp } from "firebase-admin/app";
-import { getAuth } from "firebase-admin/auth";
-import { getFirestore } from "firebase-admin/firestore";
+import admin from "firebase-admin";
 
+const projectId = process.env.FIREBASE_ADMIN_PROJECT_ID;
+const clientEmail = process.env.FIREBASE_ADMIN_CLIENT_EMAIL;
 const privateKey = process.env.FIREBASE_ADMIN_PRIVATE_KEY?.replace(/\\n/g, "\n");
 
-const app =
-  getApps().length > 0
-    ? getApps()[0]
-    : initializeApp({
-        credential: cert({
-          projectId: process.env.FIREBASE_ADMIN_PROJECT_ID,
-          clientEmail: process.env.FIREBASE_ADMIN_CLIENT_EMAIL,
-          privateKey,
-        }),
-      });
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert({
+      projectId,
+      clientEmail,
+      privateKey,
+    }),
+  });
+}
 
-export const adminAuth = getAuth(app);
-export const adminDb = getFirestore(app);
+export default admin;
