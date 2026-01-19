90ce0daba59af688c9b1a92a052fd900f6b4ed4a
Abdoubba5 <158740943+Abdoubba5@users.noreply.github.com>
2026-01-19 15:02:39 +0100

    Create lion.ts

---
+++ mirrooftrader/src/lib/lion.ts
@@ -0,0 +1,7 @@
+14:25:49.813 Running build in Washington, D.C., USA (East) – iad1
+14:25:49.814 Build machine configuration: 2 cores, 8 GB
+14:25:50.108 Cloning github.com/Abdoubba5/mirrooftrader (Branch: main, Commit: 7c27a9a)
+14:25:50.109 Previous build caches not available.
+14:25:50.573 Cloning completed: 465.000ms
+14:25:52.310 Running "vercel build"
+14:25:53.436 Vercel CLI 50.4.5
